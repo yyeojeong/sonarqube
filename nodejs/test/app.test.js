108c4aeeb69a78bac3f684b71cf4a71847dd5fe8
yyeojeong <9377duwjd@gmail.com>
2024-09-27 20:38:00 +0900

    111111

--- nodejs/test/app.test.js
+++ nodejs/test/app.test.js
@@ -17,13 +17,3 @@ describe('GET /', () => {
     expect(response.text).toBe('Hello World!');
   });
 });
-
-
-describe('GET /hello', () => {
-  it('responds with Hello World!22', async () => {
-    const response = await request(app).get('/');
-    expect(response.statusCode).toBe(200);
-    expect(response.text).toBe('Hello World!22');
-    
-  });
-});
